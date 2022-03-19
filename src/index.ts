b3076ba357c0278db93c02817b7d4c5005c009fc
Nirmal Kumar <nirmal1064@gmail.com>
2022-03-19 22:19:04 +0530

    Issue fix

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import app from "./app";
 
-const PORT = process.env.PORT || 3000;
+const PORT: number = Number(process.env.PORT) || 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("<h1>Hello from the TypeScript world!</h1>");
 });
 
 app.listen(PORT, () => {
-  console.log(`Running on PORT ${PORT}`);
+  console.log(`Running on PORT ${PORT.toString()}`);
 });
